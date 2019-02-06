0c41dd0913cdaf022db68afa8af457ed51b21200
tangweiyang <702185070@qq.com>
2019-02-06 23:17:16 +0800

    python 对excel的操作，读、写、修改，使用xlwt,xlrd,xlutil三种包

---
+++ excel_write_read_update/excel_write.py
@@ -0,0 +1,30 @@
+# encoding: utf-8
+'''
+@author: weiyang_tang
+@contact: weiyang_tang@126.com
+@file: excel_write.py
+@time: 2019-02-06 22:58
+@desc: excel表格的写入保存
+'''
+
+import xlwt
+
+def write_excel():
+    # 2. 创建Excel工作薄
+    myWorkbook = xlwt.Workbook()
+    # 3. 添加Excel工作表
+    mySheet = myWorkbook.add_sheet('A Test Sheet')
+    # 4. 写入数据
+    myStyle = xlwt.easyxf('font: name Times New Roman, color-index red, bold on', num_format_str='#,##0.00')  # 数据格式
+    #mySheet.write(i, j, 1234.56, myStyle)
+    mySheet.write(2, 0, 1)  # 写入A3，数值等于1
+    mySheet.write(2, 1, 1)  # 写入B3，数值等于1
+    mySheet.write(2, 2, xlwt.Formula("A3+B3"))  # 写入C3，数值等于2（A3+B3）
+    # 5. 保存
+    myWorkbook.save('data/excelFile.xlsx')
+
+
+if __name__ == '__main__':
+    # 写入Excel
+    write_excel();
+    print('写入成功')
--- excel_write_read_update/xlutilsDemo.py
+++ excel_write_read_update/xlutilsDemo.py
@@ -4,15 +4,14 @@
 @contact: weiyang_tang@126.com
 @file: xlutilsDemo.py
 @time: 2019-02-04 23:21
-@desc:
+@desc: 对已有的excel文件进行修改
 '''
 
 import xlrd
-import xlwt
 from xlutils.copy import copy
 
 # 打开想要更改的excel文件
-old_excel = xlrd.open_workbook('excelFile.xls', formatting_info=True)
+old_excel = xlrd.open_workbook('data/excelFile.xls', formatting_info=True)
 # 将操作文件对象拷贝，变成可写的workbook对象
 new_excel = copy(old_excel)
 # 获得第一个sheet的对象
@@ -24,6 +23,7 @@ ws.write(0, 2, '第一行，第三列')
 ws.write(1, 0, '第二行，第一列')
 ws.write(1, 1, '第二行，第二列')
 ws.write(1, 2, '第二行，第三列')
-new_excel.save('excelFile.xls')
+new_excel.save('data/excelFile.xls')
 # 另存为excel文件，并将文件命名
-new_excel.save('new_fileName.xls')
+new_excel.save('data/new_fileName.xls')
+print("excel文件修改完毕")
